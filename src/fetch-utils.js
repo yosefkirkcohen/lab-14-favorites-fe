ec1f63453fb3df459069dc7aa2abe750ba649395
ProsperieEli <ElijahProsperie@gmail.com>
2021-10-13 21:03:32 -0400

    added post function

--- src/fetch-utils.js
+++ src/fetch-utils.js
@@ -13,3 +13,14 @@ export async function login(email, password) {
         .send({ email, password})
     return response.body
 }
+
+export async function postFavorite(id, token) {
+    const response = await request
+      .post(`https://secure-springs-81342.herokuapp.com/api/favorites`)
+      .send({ id: id })
+      .set('Authorization', token);
+  
+    return response.body;
+  
+  }
+  
